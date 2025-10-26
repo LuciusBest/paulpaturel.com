bd879ebe614fea8f4f83bee6a03c421ecc2e5944
LuciusBest <paulpaturel75@gmail.com>
2025-10-27 00:50:02 +0100

    fix(ui): hide toper elements during loading state for improved UX

--- src/js/data/siteVersionInline.js
+++ src/js/data/siteVersionInline.js
@@ -1,1 +1,1 @@
-window.SiteVersion = {"semantic":"V1.00.00","timestamp":"2025/10/27/42/18","updatedAt":"2025-10-26T23:42:18.982Z"};
+window.SiteVersion = {"semantic":"V1.00.00","timestamp":"2025/10/27/49/55","updatedAt":"2025-10-26T23:49:55.059Z"};
