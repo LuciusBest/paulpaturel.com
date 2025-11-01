982b0e3d81377b2b733fb6c15a44c978889aff7c
LuciusBest <paulpaturel75@gmail.com>
2025-11-01 16:49:20 +0100

    fix(ui): remove black backdrop from topper for improved visibility

--- src/js/data/siteVersionInline.js
+++ src/js/data/siteVersionInline.js
@@ -1,2 +1,2 @@
-window.SiteVersion = {"semantic":"V1.00.01","timestamp":"2025/11/01/45/16","updatedAt":"2025-11-01T15:45:16.922Z","cacheToken":"V1000120251101451620251101T154516922Z"};
-window.__assetVersion = 'V1000120251101451620251101T154516922Z';
+window.SiteVersion = {"semantic":"V1.00.01","timestamp":"2025/11/01/49/12","updatedAt":"2025-11-01T15:49:12.693Z","cacheToken":"V1000120251101491220251101T154912693Z"};
+window.__assetVersion = 'V1000120251101491220251101T154912693Z';
