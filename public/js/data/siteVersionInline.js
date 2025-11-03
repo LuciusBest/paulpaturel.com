f51ce1ce76210bd11a15059cd57afb97d55ec94c
LuciusBest <paulpaturel75@gmail.com>
2025-11-03 16:21:59 +0100

    Public push

--- public/js/data/siteVersionInline.js
+++ public/js/data/siteVersionInline.js
@@ -1,2 +1,2 @@
-window.SiteVersion = {"semantic":"V1.00.01","timestamp":"2025/11/01/49/12","updatedAt":"2025-11-01T15:49:12.693Z","cacheToken":"V1000120251101491220251101T154912693Z"};
-window.__assetVersion = 'V1000120251101491220251101T154912693Z';
+window.SiteVersion = {"semantic":"V1.00.01","timestamp":"2025/11/03/21/31","updatedAt":"2025-11-03T15:21:31.756Z","cacheToken":"V1000120251103213120251103T152131756Z"};
+window.__assetVersion = 'V1000120251103213120251103T152131756Z';
