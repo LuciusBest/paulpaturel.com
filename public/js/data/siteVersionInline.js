c73fd8033cc38e141c1815822e0c716a9dcb0634
LuciusBest <paulpaturel75@gmail.com>
2026-01-08 17:15:41 +0100

    Bump site version to V1.00.06 and update asset cache tokens

--- public/js/data/siteVersionInline.js
+++ public/js/data/siteVersionInline.js
@@ -1,2 +1,2 @@
-window.SiteVersion = {"semantic":"V1.00.05","timestamp":"2026/01/08/13/37","updatedAt":"2026-01-08T16:13:37.416Z","cacheToken":"V1000520260108133720260108T161337416Z"};
-window.__assetVersion = 'V1000520260108133720260108T161337416Z';
+window.SiteVersion = {"semantic":"V1.00.06","timestamp":"2026/01/08/15/28","updatedAt":"2026-01-08T16:15:28.657Z","cacheToken":"V1000620260108152820260108T161528657Z"};
+window.__assetVersion = 'V1000620260108152820260108T161528657Z';
