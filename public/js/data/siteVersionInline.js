aef5bd2dd07ae13c7f97399379494cd527581dc9
LuciusBest <paulpaturel75@gmail.com>
2026-09-15 10:27:45 +0200

    Image 3D update

--- public/js/data/siteVersionInline.js
+++ public/js/data/siteVersionInline.js
@@ -1,2 +1,2 @@
-window.SiteVersion = {"semantic":"V1.00.16","timestamp":"2026/09/14/10/53","updatedAt":"2026-09-14T20:10:53.621Z","cacheToken":"V1001620260914105320260914T201053621Z"};
-window.__assetVersion = 'V1001620260914105320260914T201053621Z';
+window.SiteVersion = {"semantic":"V1.00.17","timestamp":"2026/09/15/27/27","updatedAt":"2026-09-15T08:27:27.190Z","cacheToken":"V1001720260915272720260915T082727190Z"};
+window.__assetVersion = 'V1001720260915272720260915T082727190Z';
